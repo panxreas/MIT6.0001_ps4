bb572c99b86e5b344f51b531d039a584d225c37c
panxreas <skaterpancreas@gmail.com>
2021-08-07 04:03:42 -0500

    plain text class finished

--- ps4b.py
+++ ps4b.py
@@ -113,9 +113,7 @@ class Message(object):
         
         
         plaintext = self.message_text
-        
         alt_shift = 0
-        s_output = ""
         d_output = {}
         
         if shift > len(low_case):
@@ -155,10 +153,9 @@ class Message(object):
         
         
         plaintext = self.message_text
-        
         alt_shift = 0
         s_output = ""
-        d_output = {}
+        
         
         if shift > len(low_case):
             alt_shift = shift % len(low_case)
@@ -191,15 +188,18 @@ class PlaintextMessage(Message):
             self.message_text_encrypted (string, created using shift)
 
         '''
-        pass #delete this line and replace with your code here
-
+        Message.__init__(self, text)
+        self.shift = shift
+        self.encryption_dict = text.build_shift_dict(shift) 
+        self.message_text_encrypted = text.apply_shift(shift)
+        
     def get_shift(self):
         '''
         Used to safely access self.shift outside of the class
         
         Returns: self.shift
         '''
-        pass #delete this line and replace with your code here
+        return self.shift
 
     def get_encryption_dict(self):
         '''
@@ -207,7 +207,7 @@ class PlaintextMessage(Message):
         
         Returns: a COPY of self.encryption_dict
         '''
-        pass #delete this line and replace with your code here
+        return self.encryption_dict
 
     def get_message_text_encrypted(self):
         '''
@@ -215,7 +215,7 @@ class PlaintextMessage(Message):
         
         Returns: self.message_text_encrypted
         '''
-        pass #delete this line and replace with your code here
+        return self.message_text_encrypted
 
     def change_shift(self, shift):
         '''
@@ -227,7 +227,7 @@ class PlaintextMessage(Message):
 
         Returns: nothing
         '''
-        pass #delete this line and replace with your code here
+        self.shift = shift
 
 
 class CiphertextMessage(Message):
@@ -279,7 +279,7 @@ if __name__ == '__main__':
     m1 = "Encrypt me plz!"
     
     cypher = Message(m1)
-    print((cypher.build_shift_dict(2)).keys())
-    print((cypher.build_shift_dict(2)).values())
+    print((cypher.build_shift_dict(50)).keys())
+    print((cypher.build_shift_dict(50)).values())
     print('\n')
-    print(cypher.apply_shift(2))
+    print(cypher.apply_shift(50))
